3ecfc694213d5a45f87c36abdb34df663ee8057a
Matison <matison14@gmail.com>
2018-04-16 23:02:39 +0200

    Initial commit

---
+++ Assets/Scripts/pickupcoin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class pickupcoin : MonoBehaviour {
+
+	public Text wynik;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Player")) {
+			Destroy(this.gameObject);
+			Debug.Log("Coin was picked up");
+		}
+	}
+}
